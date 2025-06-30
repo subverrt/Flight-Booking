6a039fa4cfe507533652c334085a527740b0eaf9
subverrt <subhamdash265@gmail.com>
2025-06-30 17:51:53 +0530

    Update BookingForm.jsx

--- frontend/src/Components/BookingForm/BookingForm.jsx
+++ frontend/src/Components/BookingForm/BookingForm.jsx
@@ -94,7 +94,7 @@ const BookingForm = () => {
         handler: async (razorpayResponse) => {
           try {
             await axios.post(
-              `${baseURL}/bookings/verify`,
+              `${baseURL}/auth/bookings/verify`,
               {
                 razorpayPaymentId: razorpayResponse.razorpay_payment_id,
                 razorpayOrderId: razorpayResponse.razorpay_order_id,
